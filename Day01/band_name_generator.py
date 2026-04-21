888d68bc25d2b2acbd5a9993de5c0278b6135251
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-21 03:16:10 -0400

    Update band_name_generator.py

--- Day01/band_name_generator.py
+++ Day01/band_name_generator.py
@@ -1,3 +1,4 @@
+# Day 1 - Band Name Generator Project
 print("Welcome to the Band Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")

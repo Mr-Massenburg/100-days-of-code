4ea45a2423869ff535519857272d53e6039d19e3
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-29 03:50:23 -0400

    Create debug_missing_value.py

---
+++ Day13/debug_missing_value.py
@@ -0,0 +1,15 @@
+# Day 13 | Debugging Practice
+
+# Fixed Code
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# Original Code
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+

7e5123003b7c28812e24d06b1a8ed566d96a81d7
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-29 03:39:37 -0400

    Create debug_odd_even.py

---
+++ Day13/debug_odd_even.py
@@ -0,0 +1,15 @@
+# Day 13 Debugging practice
+
+# Fixed Code
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+  # Original Code
+  # def odd_or_even(number):
+  #   if number % 2 = 0:
+  #       return "This is an even number."
+  #   else:
+  #       return "This is an odd number."

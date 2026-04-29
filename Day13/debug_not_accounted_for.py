6b7937705c199cf67bb9b3cbac3c9ee8f2cfb073
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-29 03:47:27 -0400

    Create debug_not_accounted_for.py

---
+++ Day13/debug_not_accounted_for.py
@@ -0,0 +1,18 @@
+# Day 13 | Debugging Practice
+
+# Fixed code
+year = int(input("What's your year of birth?"))
+
+if year > 1980 and year < 1994:
+    print("You are a millennial.")
+elif year >= 1994:
+    print("You are a Gen Z.")
+
+# Original Code
+# year = int(input("What's your year of birth?"))
+
+# if year > 1980 and year < 1994:
+#     print("You are a millennial.")
+# elif year > 1994:
+#     print("You are a Gen Z.")
+

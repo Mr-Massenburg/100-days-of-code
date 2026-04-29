865c2adb4a647b7ad8b6faec942af13e9e1e2a4c
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-29 03:44:43 -0400

    Create debug_find_issue.py

---
+++ Day13/debug_find_issue.py
@@ -0,0 +1,19 @@
+# Day 13 | Debugging Practice
+
+# Fixed code
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Original Code
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
+
+
+# my_function()

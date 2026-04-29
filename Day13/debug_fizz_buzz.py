d81c2ceb22a778c997535b62686f50a117516057
Mr-Massenburg <marquis.massen@gmail.com>
2026-04-29 03:42:49 -0400

    Create debug_fizz_buzz.py

---
+++ Day13/debug_fizz_buzz.py
@@ -0,0 +1,25 @@
+# Day 13 | Debugging practice
+
+# Fixed Code
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+# Original Code
+# def fizz_buzz(target):
+#     for number in range(1, target + 1):
+#         if number % 3 == 0 or number % 5 == 0:
+#             print("FizzBuzz")
+#         if number % 3 == 0:
+#             print("Fizz")
+#         if number % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print([number])

beee014b4fda3eebc22b8db78384334fa42b62c5
Mr-Massenburg <marquis.massen@gmail.com>
2026-05-01 02:27:44 -0400

    Create coffee_machine_oop.py

---
+++ Day16/coffee_machine_oop.py
@@ -0,0 +1,27 @@
+#Day 16 | Coffee Machine with OOP
+
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+machine_running = True
+
+while machine_running:
+    order = input(f"What would you like? ({menu.get_items()}):").lower()
+
+    if order == "off":
+        machine_running = False
+    elif order == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        enough_resources = coffee_maker.is_resource_sufficient(drink)
+        if enough_resources:
+            valid_payment = money_machine.make_payment(drink.cost)
+            if valid_payment:
+                coffee_maker.make_coffee(drink)

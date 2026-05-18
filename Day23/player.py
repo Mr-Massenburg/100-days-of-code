51153066d32367bb76cd91a038c88ab9ee5268b3
Mr-Massenburg <marquis.massen@gmail.com>
2026-05-18 14:40:22 -0400

    Create player.py

---
+++ Day23/player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setpos(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_forward(self):
+        self.forward(MOVE_DISTANCE)
+
+    def cross_finish_line(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def level_complete(self):
+        self.setpos(STARTING_POSITION)
